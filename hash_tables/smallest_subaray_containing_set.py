aa6b2efa4440ff80ca06af04e3e4bbc540eefe0f
Parv Desai <desaiparv5@gmail.com>
2023-09-17 19:33:44 +0530

    substring

--- hash_tables/smallest_subaray_containing_set.py
+++ hash_tables/smallest_subaray_containing_set.py
@@ -26,10 +26,48 @@ def smallest_subarray_containing_set(text, substring):
     return min_window
 
 
+def smallest_subarray_containing_all_elements(text):
+    substring = set(text)
+    return smallest_subarray_containing_set(text, substring)
+
+
+def rearrange_text_for_max_substring_of_distinct_chars(text):
+    frequency_count = Counter(text)
+    ordered_text = ""
+    while len(frequency_count) > 1:
+        char, freq = frequency_count.most_common(1)[0]
+        ordered_text += char * freq
+        frequency_count.pop(char)
+    char, freq = frequency_count.most_common(1)[0]
+    ordered_text = char * freq + ordered_text
+    return ordered_text
+
+
+def rearrange_letters_equidistant(text, k):
+    char_freq = Counter(text)
+    ordered_text = [None] * len(text)
+    filled_index = 0
+    while char_freq:
+        char, freq = char_freq.most_common(1)[0]
+        curr_index = filled_index
+        for _ in range(freq):
+            ordered_text[curr_index] = char
+            curr_index += k
+        char_freq.pop(char)
+        filled_index += 1
+        if filled_index < len(text) - 1 and ordered_text[filled_index]:
+            while ordered_text[filled_index]:
+                filled_index += 1
+    return "".join(char for char in ordered_text if char)
+
+
 def main():
     text = "ADOBECODEBANC"
     substring = "ABC"
     print(smallest_subarray_containing_set(text, substring))
+    print(smallest_subarray_containing_all_elements(text))
+    print(rearrange_text_for_max_substring_of_distinct_chars(text))
+    print(rearrange_letters_equidistant(text, 3))
 
 
 if __name__ == "__main__":
